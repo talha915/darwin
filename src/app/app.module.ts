e9180fb4964dad652bd959676729012ec064205e
talha915 <talhazafar915426@gmail.com>
2018-05-28 15:45:33 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RightbodyComponent } from './rightbody/rightbody.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    RightbodyComponent
   ],
   imports: [
     BrowserModule
