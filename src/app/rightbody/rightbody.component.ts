55b1d3ea650db554d9b81ac15dcf7aec3de9f786
talha915 <talhazafar915426@gmail.com>
2018-06-04 13:03:17 +0500

    1.03pm

--- src/app/rightbody/rightbody.component.ts
+++ src/app/rightbody/rightbody.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rightbody.component.css']
 })
 export class RightbodyComponent implements OnInit {
-  private itemSelected:Number = 0;
+  private itemSelected:number = 0;
   constructor() { }
 
   ngOnInit() {
   }
   myfunct(){
-    this.itemSelected=1;
+    this.itemSelected= this.itemSelected + 1;
     console.log(this.itemSelected);
     return this.itemSelected;
   }
