8235d6c85bed17090cb415d657042d69b00f861f
talha915 <talhazafar915426@gmail.com>
2018-06-05 16:23:55 +0500

    again

---
+++ src/app/Data/Folder.ts
@@ -0,0 +1,20 @@
+export const Data=[
+    {
+        folder: 'My Models',
+        subfolder: 'Folder 1',
+        subfolder1: 'Folder 2'
+    },
+    {
+        folder: 'Folder 3',
+    },
+    {
+        folder: 'Folder 4',
+        subfolder: 'Folder 5'
+    },
+    {
+        folder: 'Folder 6',
+        subfolder: 'Folder 7',
+        subfolder1: 'Folder 8',
+        subfolder2: 'Folder 9'
+    }
+];
--- src/app/dialogue-component/dialogue-component.component.ts
+++ src/app/dialogue-component/dialogue-component.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogComponent, DialogService } from "ng2-bootstrap-modal";
+import { Data } from '../Data/Folder';
 @Component({
   selector: 'app-dialogue-component',
   templateUrl: './dialogue-component.component.html',
   styleUrls: ['./dialogue-component.component.css']
 })
 export class DialogueComponentComponent extends DialogComponent<null, null> implements OnInit{
+  folders: Array<any>=Data;
   constructor(dialogService: DialogService) {
     super(dialogService);
   }
