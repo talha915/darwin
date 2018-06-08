604d03be4d31b4da85624924ccb981d5d56d9c9a
talha915 <talhazafar915426@gmail.com>
2018-06-08 12:41:17 +0500

    fixing

--- src/app/Data/Folder.ts
+++ src/app/Data/Folder.ts
@@ -1,4 +1,4 @@
-export const Data=[
+/*export const Data=[
     {
         folder: 'My Models',
         subfolder: 'Folder 1',
@@ -17,4 +17,39 @@ export const Data=[
         subfolder1: 'Folder 8',
         subfolder2: 'Folder 9'
     }
+];*/
+
+
+export const Data=[
+    {
+        folder: 'My Models',
+        subfolders: [
+            {
+                subfolder: 'Folder 1',
+                subfolder1: 'Folder 2'
+            }
+        ]
+    },
+    {
+        folder: 'Folder 3'
+    },
+    {
+        folder: 'Folder 4',
+        subfolders: [
+            {
+                subfolder: 'Folder 5'
+            }
+        ]
+    },
+    {
+        folder: 'Folder 6',
+        subfolders:[
+            {
+                subfolder: 'Folder 7',
+                subfolder1: 'Folder 8',
+                subfolder2: 'Folder 9'
+            }
+        ]
+    }
 ];
+
